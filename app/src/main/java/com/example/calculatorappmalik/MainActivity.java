191bc9d982e74cbbdb7424a973fc228ab9f2d3fa
BMalik227 <113694398+BMalik227@users.noreply.github.com>
2023-09-08 11:05:02 -0500

    added divison

--- app/src/main/java/com/example/calculatorappmalik/MainActivity.java
+++ app/src/main/java/com/example/calculatorappmalik/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorappmalik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,73 +16,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     String showEquation ="";
-    public void button1Clicked(){
-        showEquation+="1";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button2Clicked(){
-        showEquation+="2";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button3Clicked(){
-        showEquation+="3";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button4Clicked(View v){
-        showEquation+="4";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button5Clicked(){
-        showEquation+="5";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button6Clicked(){
-        showEquation+="6";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button7Clicked(){
-        showEquation+="7";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button8Clicked(){
-        showEquation+="8";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void button0Clicked(){
-        showEquation+="0";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void buttonPlusClicked(){
-        showEquation+="+";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void buttonMinusClicked(){
-        showEquation+="-";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void buttonDivideClicked(View v){
-        showEquation+="÷";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void buttonMultiplyClicked(){
-        showEquation+="x";
-        TextView output = findViewById(R.id.output);
-        output.setText(showEquation);
-    }
-    public void buttonEqualClicked(){
-        showEquation+="9";
+
+    public void buttonClicked(View v){
+        if(v.getId() == R.id.button3){
+            showEquation+="1 ";
+        }
+        if(v.getId() == R.id.buttonDivide){
+            showEquation+="÷ ";
+        }
+        if(v.getId() == R.id.button13){
+            showEquation+="4 ";
+        }
+        if(v.getId() == R.id.button24){
+            double answer = 0;
+            String temp = showEquation;
+            Log.i("denna", temp);
+            if(temp.indexOf("÷") >= 0){
+                if (temp.indexOf("÷") >= 2){
+                    String temp1 = temp.substring(temp.indexOf("÷") - 2, temp.indexOf("÷")-1);
+                    Log.i("denna", temp1);
+                    String temp2 = temp.substring(temp.indexOf("÷")+2,temp.indexOf("÷")+3);
+                    Log.i("denna", temp2);
+                    answer = Double.parseDouble(temp1) / Double.parseDouble(temp2);
+                }
+            }
+            showEquation+="= " + answer + "\n";
+            //ADD EXTRA STUFF HERE FOR SOLVING
+        }
+        if(v.getId() == R.id.button20){
+            showEquation+="- ";
+        }
+        if(v.getId() == R.id.button15){
+            showEquation+="6 ";
+        }
+        if(v.getId() == R.id.button12){
+            showEquation+="3 ";
+        }
+        if(v.getId() == R.id.button16){
+            showEquation+="7 ";
+        }
+        if(v.getId() == R.id.button18){
+            showEquation+="9 ";
+        }
+        if(v.getId() == R.id.button14){
+            showEquation+="5 ";
+        }
+        if(v.getId() == R.id.button21){
+            showEquation+="x ";
+        }
+        if(v.getId() == R.id.button19){
+            showEquation+="+ ";
+        }
+        if(v.getId() == R.id.button11){
+            showEquation+="2 ";
+        }
+        if(v.getId() == R.id.button17){
+            showEquation+="8 ";
+        }
+        if(v.getId() == R.id.button22){
+            showEquation+="0 ";
+        }
         TextView output = findViewById(R.id.output);
         output.setText(showEquation);
     }
